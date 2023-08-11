cfca63d31f4296854f5b7ef0b25b9d034607a088
tom <chings228@gmail.com>
2023-08-11 14:36:51 +0800

    Initial Commit

---
+++ NewNumberSpeaking/NewNumberSpeakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewNumberSpeakingApp.swift
+//  NewNumberSpeaking
+//
+//  Created by man ching chan on 11/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewNumberSpeakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
